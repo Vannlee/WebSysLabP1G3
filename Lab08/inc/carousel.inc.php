84a3f35ade3fc8876b19469340e2ef01bd98b19d
Vannlee <jovanlee26@gmail.com>
2025-03-14 12:47:45 +0800

    updated carousel

---
+++ Lab08/inc/carousel.inc.php
@@ -0,0 +1,56 @@
+<!-- Membership Carousel-->
+<div id = "gymCarousel" class = "carousel slide" data-bs-ride = "carousel">
+    <!-- Carousel Indicators -->
+    <ol class = "carousel-indicators">
+        <li data-bs-target = "#gymCarousel" data-bs-slide-to = "0" class = "active"></li>
+        <li data-bs-target = "#gymCarousel" data-bs-slide-to = "1"></li>
+        <li data-bs-target = "#gymCarousel" data-bs-slide-to = "2"></li>
+    </ol>
+    
+    <!-- Carousel Inner -->
+    <div class = "carousel-inner">
+        
+        <!-- First Slide -->
+        <div class = "carousel-item active"> 
+            <a href = "membership.php">
+                <img src = "images/basic_membership.png" class="d-block w-100" alt="Basic Membership">
+            </a>
+            <div class = "carousel-caption d-none d-md-block">
+                <h5>Basic Membership</h5>
+                <p>Access to gym equipment and group fitness classes.</p>
+            </div>
+        </div>
+        
+        <!-- Second Slide -->
+        <div class = "carousel-item">
+            <a href = "membership.php">
+                <img src = "images/premium_membership.jpg" class="d-block w-100" alt="Premium Membership">
+            </a>
+            <div class = "carousel-caption d-none d-md-block">
+                <h5>Premium Membership</h5>
+                <p>Access to gym equipment and group fitness classes.</p>
+            </div>
+        </div>
+    
+        <!-- Third Slide -->
+        <div class = "carousel-item">
+            <a href = "membership.php">
+                <img src = "images/supreme_membership.jpg" class="d-block w-100" alt="Supreme Membership">
+            </a>
+            <div class = "carousel-caption d-none d-md-block">
+                <h5>Supreme Membership</h5>
+                <p>Access to gym equipment and group fitness classes.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Carousel Controls -->
+    <a class="carousel-control-prev" href="#gymCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#gymCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
