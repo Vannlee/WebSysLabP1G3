656797b4950a2b13c0325413fb3a89b637964996
eeugenewyj <eugene.ong2002@gmail.com>
2025-03-14 11:31:43 +0800

    about us

--- Lab08/inc/nav.inc.php
+++ Lab08/inc/nav.inc.php
@@ -12,6 +12,10 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                 <li class="nav-item">
+                    <a class="nav-link" href="aboutus.php">About Us</a>
+                </li>
+
+                <li class="nav-item">
                     <a class="nav-link" href="location.php">Locations</a>
                 </li>
 
