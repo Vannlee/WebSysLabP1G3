14d186ed73dd4dfd2e2c6de87688ee2bcc1e38b1
eeugenewyj <eugene.ong2002@gmail.com>
2025-03-14 12:35:36 +0800

    about us

---
+++ Lab08/aboutus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Fitness Gym</title>
+        <?php
+            include "inc/head.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+            include "inc/nav.inc.php";
+        ?>
+
+        <main class="container">
+            <section id="aboutus">
+                <h1>About Us</h1>
+                <p>
+                    Fitness Gym is a chain of gyms located in various parts of Singapore. We aim to provide a comfortable and affordable gym experience for all our members. Our gyms are equipped with the latest gym equipment and our trainers are certified professionals who are always ready to help you achieve your fitness goals.
+                </p>
+                <p>
+                    We offer a wide range of classes to cater to different fitness levels and interests. Whether you are a beginner looking to get started on your fitness journey or an experienced gym-goer looking to take your workouts to the next level, we have something for everyone.
+                </p>
+                <p>
+                    At Fitness Gym, we believe that fitness should be fun and enjoyable. That's why we have created a welcoming and inclusive environment where everyone is welcome. So come join us and start your fitness journey today!
+                </p>
+            </section>
+        </main>
+        <?php
+            include "inc/footer.inc.php";
+        ?>
+    </body>
+</html>
