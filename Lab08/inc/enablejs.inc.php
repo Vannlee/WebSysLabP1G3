d005d0a0abdfb090e77abf5830805cd076ed8ca9
eeugenewyj <eugene.ong2002@gmail.com>
2025-03-25 15:03:25 +0800

    enablejs script

---
+++ Lab08/inc/enablejs.inc.php
@@ -0,0 +1,10 @@
+<noscript>
+            <style>
+                #main-content {
+                    display: none !important;
+                }
+            </style>
+            <div style="text-align: center; padding: 2rem; color: red; font-weight: bold;">
+                ⚠ JavaScript is disabled in your browser. Please enable it to use this site.
+            </div>
+        </noscript>
