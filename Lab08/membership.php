932e19f3c834a4a72ebab5f9e14cdc18d0f796a6
eeugenewyj <eugene.ong2002@gmail.com>
2025-03-25 15:05:52 +0800

    added 3 sites

---
+++ Lab08/membership.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Fitness Gym</title>
+        <?php
+            session_start();
+            include "inc/head.inc.php";
+            include "inc/enablejs.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+            include "inc/nav.inc.php";
+        ?>
+
+        <main class="container">
+            <h1>Work in Progress</h1>
+        </main>
+        <?php
+            include "inc/footer.inc.php";
+        ?>
+    </body>
+</html>
