552c1d7706ddabbc2b888b0320e3ac24e41a14de
eeugenewyj <eugene.ong2002@gmail.com>
2025-03-30 16:24:31 +0800

    bootstrap icons

--- Lab08/inc/head.inc.php
+++ Lab08/inc/head.inc.php
@@ -7,6 +7,9 @@ rel="stylesheet"
 integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
 crossorigin="anonymous">
 
+<!-- bootstrap icons -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
 <!-- bootstrap js -->
 <script defer
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
