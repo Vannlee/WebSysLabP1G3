a19aec53a0a7cfa561b7d07c1779a8a1ad2063bd
eeugenewyj <eugene.ong2002@gmail.com>
2025-04-02 10:55:15 +0800

    rename

--- Lab08/booking.php
+++ Lab08/booking.php
@@ -41,7 +41,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Fitness Gym</title>
+        <title>Bookings - Gymbros</title>
         <?php
             include "inc/head.inc.php";
         ?>
