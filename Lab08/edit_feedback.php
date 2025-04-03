cb5067c7b5c04b65b50686066cee6ecbe31eb2cd
Vannlee <jovanlee26@gmail.com>
2025-04-04 02:31:04 +0800

    completed feedback update with user verification

--- Lab08/edit_feedback.php
+++ Lab08/edit_feedback.php
@@ -21,57 +21,62 @@
     } else {
         $id = $_SESSION['user_id'];
     }
+
+    if (!isset($_GET['id'])) {
+            header("Location: feedback.php");
+            exit();
+    }
     
     if (isset($_GET['id']) && isLoggedIn()) {
         // User is logged in and feedback id to be updated is present
         $f_id = $_GET['id'];
 
         $stmt = $conn->prepare("SELECT member_id FROM Gymbros.membership_feedback WHERE feedback_id = ?");
-        $stmt->bind_param("i",  $feedback_id);
+        $stmt->bind_param("i",  $f_id);
         $stmt->execute();
         $result = $stmt->get_result();
                 
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
         
-            if ($user_id != $row["member_id"]) {
-                $stmt->close();
-                $conn->close();
+            if ($id != $row["member_id"]) {
                 header("Location: feedback.php");
                 exit();
             }
-        }
-    } else {
-        header("Location: feedback.php");
-        exit();
-    }
-?>
+            else {
+                include "inc/head.inc.php";
+                include "inc/nav.inc.php";
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Update Feedback - Gymbros</title>
-    <?php
-        include "inc/head.inc.php";
-    ?>
-</head>
-<body>
-    <?php 
-        include "inc/nav.inc.php";
-    ?>
-    
-    <main class="container">
-        <form action="update_feedback.php" method="post">
-            <input type="hidden" name="feedback_id" value="<?php htmlspecialchars($f_id) ?>">
-            <input type="hidden" name="user_id" value="<?php htmlspecialchars($id) ?>">
-            <div class="alert alert-light mt-4">
-                <h1>Update Feedback</h1>
-                <label for="feedback_content" class="form-label">New Content:</label>
-                <textarea class="form-control" id="feedback_content" name="feedback_content" rows="5" required></textarea>
-            </div>
-            <button type="submit" name="update_feedback" class="btn btn-success" style="float: right;">Update</button>
-        </form>
-    </main>
-    <?php include "inc/footer.inc.php"; ?>
-    </body>
-</html>
+                echo "<title>Update Feedback</title>";
+                echo "<main class='container'>";
+                echo '<form action="update_feedback.php" method="post">
+                        <input type="hidden" name="feedback_id" value="' . htmlspecialchars($f_id) . '">
+                        <input type="hidden" name="user_id" value="' . htmlspecialchars($id) . '">
+                        <div class="alert alert-light mt-4">
+                            <h1>Update Feedback</h1>
+                            <label for="feedback_content" class="form-label">New Content:</label>
+                            <textarea class="form-control" id="feedback_content" name="feedback_content" rows="5" required></textarea>
+                        </div>
+                        <button type="submit" name="update_feedback" class="btn btn-success" style="float: right;">Update</button>
+                    </form>';
+            }
+        }
+        else {
+            include "inc/head.inc.php";
+            include "inc/nav.inc.php";
+            
+            echo "<title>No Record Found</title>";
+            echo "<main class='container'>";
+            echo "<div class='alert alert-danger mt-4'>";
+            echo "<h3>No Feedback Record Found</h3>";
+            echo "<p>This is no such record for this feedback, kindly return to the feedback page and try again.</p>";
+            echo "</div>";
+            echo "<p><a class='btn btn-primary' href='feedback.php'>Back to Feedback Records</a></p></main>";
+        }
+        
+        $stmt->close();
+        $conn->close();
+    }
+
+    include "inc/footer.inc.php";
+?>
